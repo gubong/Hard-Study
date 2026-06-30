5b34d61b71a4cc2e3d63232dff9c3206379b43a0
GGB\fkqtm <fkqtmxk789@gmail.com>
2026-06-30 23:48:43 +0900

    JS function, JAVA start

---
+++ Test.java
@@ -0,0 +1,18 @@
+package Abc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//java file(한글)>comfile(숫자)>bin저장>pc가 읽음. java는 comfile언어
+		//object=설계도> 이용해서코딩>java는 객체지향언어.
+		
+		System.out.print("java start~");
+		byte num1 = -128; //byte = -128~127
+		short num2 = 32767; 
+		int num3 = 2147483647;
+		long num4 = 9223372036854775807L;
+		//정수 변수 만드는 법. 각각 1,2,4,8 byte의 크기제한.
+		//정수의 대표타입은 int (범용성때문.)
+	}
+
+}
