b94896e792a8f9243c2e7f797a7c5af0fc991703
gubong <105339056+gubong@users.noreply.github.com>
2026-07-22 19:39:36 +0900

    select-arraylist

---
+++ 26.07.22.select2.java
@@ -0,0 +1,60 @@
+package o_database;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Snack {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		SnackDao dao = new SnackDao();
+		int gubun =0;
+		do {
+			System.out.print("검색 제품코드:1, 제품명:2, 제조사:3, 전체출력4, 종료:0");
+			gubun = sc.nextInt();
+			if(gubun==1) {
+				System.out.println("조회할제품코드?");
+				String code = sc.next();	//완벽히일치하는놈만찾기
+				SnackDto dto = dao.getSnackInfo(code);
+				if(dto==null) {
+					System.out.println("없는코드임ㅇㅇ");
+				} else {
+					dao.snackPrint(dto);
+				}
+				
+			}else if(gubun==2) {		//제품명 뭐 넣으면 들어가있는제품명 쭈르륵다나와라
+				System.out.println("제품명?");
+				String searchName = sc.next();
+				ArrayList<SnackDto> arr = dao.getSnackList(searchName);
+				if(arr.size()==0) {
+					System.out.println("제품정보없음");
+				}else {
+					dao.snackPtintAll(arr);
+				}
+				
+				
+			}else if(gubun==3) {
+				System.out.println("제조사 농심:1 롯데:2 해태:3 오리온:4 ");
+				String searchCompany = sc.next();
+				if(searchCompany.equals("1")||searchCompany.equals("2")||searchCompany.equals("3")||searchCompany.equals("4")) {
+					ArrayList<SnackDto> arr = dao.getSnackCompany(searchCompany);
+					
+					if(arr.size() == 0) System.out.println("111");
+					dao.snackPtintAll(arr);
+				}
+				
+			}else if(gubun==4) {
+				
+			}
+			
+			
+		
+		}while(gubun !=0);		
+		
+		System.out.println("system 종뇨^6 :D");
+		
+		
+		
+	}
+
+}
