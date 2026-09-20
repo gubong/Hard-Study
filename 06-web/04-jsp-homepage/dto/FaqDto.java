e486491b3a8569c85d94ce3b93b4fcb531e3b5ec
gubong <105339056+gubong@users.noreply.github.com>
2026-09-20 21:11:57 +0900

    Add files via upload

---
+++ 06-web/04-jsp-homepage/dto/FaqDto.java
@@ -0,0 +1,37 @@
+package dto;
+
+public class FaqDto {
+	String no,question,answer,reg_id,reg_date;
+
+	
+	public FaqDto(String no, String question, String answer, String reg_id, String reg_date) {
+		this.no = no;
+		this.question = question;
+		this.answer = answer;
+		this.reg_id = reg_id;
+		this.reg_date = reg_date;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public String getReg_id() {
+		return reg_id;
+	}
+
+	public String getReg_date() {
+		return reg_date;
+	}
+	
+	
+
+}
